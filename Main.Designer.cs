960b318d87bf733e0831693b1ab5819a68a1fa18
alex mason <alexmas9n@gmail.com>
2022-03-27 18:28:25 +0300

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace USBFormatingWithWinForm
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(45, 81);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(258, 28);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 187);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Main
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace USBFormatingWithWinForm {
+    public partial class Main : Form {
+        public Main() {
+            InitializeComponent();
+        }
+
+        public string GetSize(long size) {
+            string postfix = "Bytes";
+            long result = size;
+            if (size >= 1073741824) { // Larger than 1 GB
+                result = size / 1073741824;
+                postfix = "GB";
+            } else if (size >= 1048576) { // Larger than 1 MB
+                result = size / 1048576;
+                postfix = "MB";
+            } else if (size >= 1024) { // Larger than 1 KB
+                result = size / 1024;
+                postfix = "KB";
+            }
+            return result.ToString("F1") + " " + postfix;
+        }
+
+        private void Main_Load(object sender, EventArgs e) {
+            try {
+                DriveInfo[] Removeable = DriveInfo.GetDrives();
+                foreach (DriveInfo r in Removeable) {
+                    if (r.DriveType == DriveType.Removable) {
+                        string DriverSize = GetSize(r.TotalSize);
+
+                    }
+                }
+            } catch { MessageBox.Show("Error Fetching Removeable Drives", "Error"); }
+        }
+    }
+}
